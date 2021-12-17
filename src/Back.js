a2143112fd95562d5b10c6a1b17dbeff017c8de3
“rouzbehmozafari” <rouzbeh.mozafari@yahoo.com>
2021-12-17 16:37:48 +0100

    complete

--- src/Back.js
+++ src/Back.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import back from './images/Back.svg'
 const Back = () => {
     return ( <div className="Back">
-        <Link to='/All' ><img className='backimg' src={back} alt="back" /></Link>
+        <Link to='../' ><img className='backimg' src={back} alt="back" /></Link>
     </div> );
 }
  
--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import all from './images/all.jpg'
 import random from './images/random.png'
 import { Link } from 'react-router-dom';
-
+import {card} from './DataBase'
  
 class Home extends React.Component {
   state = {
@@ -10,6 +10,9 @@ class Home extends React.Component {
   }
 
   render() { 
+
+    let randomC = card[(Math.round(Math.random()*card.length))]._id
+
     return (
       <div className="Home">
         <Link to='/All' ><div className="all">
@@ -17,11 +20,11 @@ class Home extends React.Component {
           <div className="yellarea"><p>All Beers</p></div>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe voluptas neque explicabo nesciunt repellat voluptate.</p>
         </div></Link>
-        <div className="all">
+        <Link to={`/All/${randomC}`}><div className="all">
           <img src={random} alt="random" />
           <div className="yellarea"><p>Random Beer</p></div>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe voluptas neque explicabo nesciunt repellat voluptate.</p>
-        </div>
+        </div></Link>
       </div>
     );
   }
